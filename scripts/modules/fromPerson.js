52eb3e1696cd0d8f33f42897a0da6709d1ff6023
Parit <wellbiz@ya.ru>
2022-02-02 09:02:19 +0300

    update code

--- scripts/modules/fromPerson.js
+++ scripts/modules/fromPerson.js
@@ -1,66 +1,66 @@
-import createElement from './createElements.js';
+import createElement from "./createElements.js";
 
 const createFormPerson = (n) => {
-    const form = createElement('form', {
-        className: 'person',
-    });
+  const form = createElement("form", {
+    className: "person",
+  });
 
-    const title = createElement('h2', {
-        className: 'person__title',
-        textContent: `Пассажир #${n++}`,
-    });
+  const title = createElement("h2", {
+    className: "person__title",
+    textContent: `Пассажир #${n++}`,
+  });
 
-    const fieldName = createElement('div', {
-        className: 'field',
-    });
-    const labelName = createElement('label', {
-        className: 'field__label',
-        for: `name${n}`,
-        textContent: 'ФИО',
-    });
+  const fieldName = createElement("div", {
+    className: "field",
+  });
+  const labelName = createElement("label", {
+    className: "field__label",
+    for: `name${n}`,
+    textContent: "ФИО",
+  });
 
-    const inputName = createElement('input', {
-        className: 'field__input',
-        id: `name${n}`,
-        name: 'name',
-        type: 'text',
-        placeholder: 'Введите ваше ФИО',
-        required: true,
-    });
-    const labelTicket = createElement('label', {
-        className: 'field__label',
-        for: `ticket${n}`,
-        textContent: 'Номер билета (10 цифр)',
-    });
+  const inputName = createElement("input", {
+    className: "field__input",
+    id: `name${n}`,
+    name: "name",
+    type: "text",
+    placeholder: "Введите ваше ФИО",
+    required: true,
+  });
+  const labelTicket = createElement("label", {
+    className: "field__label",
+    for: `ticket${n}`,
+    textContent: "Номер билета (10 цифр)",
+  });
+  const inputTicket = createElement("input", {
+    className: "field__input",
+    id: `ticket${n}`,
+    name: "ticket",
+    placeholder: "Номер билета",
+    minLength: "10",
+    maxLength: "10",
+  });
 
-    const inputTicket = inputName.cloneNode(false);
-    inputTicket.className = 'field__input';
-    inputTicket.id = `ticket${n}`;
-    inputTicket.name = 'ticket';
-    inputTicket.placeholder = 'Номер билета';
-    inputTicket.setAttribute('minlength', '10');
-    inputTicket.setAttribute('maxlength', '10');
+  fieldName.append(labelName, inputName);
+  const fieldTicket = fieldName;
+  fieldTicket.append(labelTicket, inputTicket);
 
-    fieldName.append(labelName, inputName);
-    const fieldName2 = fieldName;
-    fieldName2.append(labelTicket, inputTicket);
+  const button = createElement("button", {
+    className: "btn-confirm",
+    type: "submit",
+    textContent: "Подтвердить",
+  });
+  form.append(title, fieldName, fieldTicket, button);
 
-    const button = createElement('button', {
-        className: 'btn-confirm',
-        type: 'submit',
-        textContent: 'Подтвердить',
-    });
-    form.append(title, fieldName, fieldName2, button);
-
-    return form;
+  return form;
 };
 
 const getFormPerson = (count) => {
-    const forms = [];
-    if (count > 6) count = 6;
-    for (let i = 0; i < count; ++i) {
-        forms.push(createFormPerson(i));
-    }
-    return forms;
+  const forms = [];
+  if (count > 6) count = 6;
+  for (let i = 0; i < count; ++i) {
+    forms.push(createFormPerson(i));
+  }
+  return forms;
 };
 export default getFormPerson;
